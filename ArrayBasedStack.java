d71bc06c2b784895f697a593e68c8662c3c3e1df
Adrian Manzano <manzanoa@egr-maclab-20.local>
2018-02-15 14:59:25 -0500

    added changes

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 public class ArrayBasedStack<T> implements StackInterface<T>
 {
-   final int INITIAL_CAPACITY = 5;
+   
+   final int INITIAL_CAPACITY;
    int topOfStack = 0;
-   T[] data = (T[]) new Object[INITIAL_CAPACITY];
    boolean initiallized = false;
+   T[] data;
    
    public ArrayBasedStack(int i)
    {
@@ -11,9 +13,56 @@ public class ArrayBasedStack<T> implements StackInterface<T>
       {
          throw new IllegalArgumentException("Stack cannot be negative.");
       }  
+      topOfStack = -1;
+      INITIAL_CAPACITY = i;
+      data = (T[]) new Object[INITIAL_CAPACITY];
+
       
    }
    
+   public ArrayBasedStack()
+   {
+      INITIAL_CAPACITY = 5;
+      data = (T[]) new Object[INITIAL_CAPACITY];
 
+   }
    
+   private T[] doubleArraySize(T[] data1)
+   {
+      T[] copy = Arrays.copyOf(data1,(data1.length*2));
+      return copy;
+   }
+   
+   public void push(T newEntry)
+   {
+      
+   }
+
+   public T pop()
+   {
+         
+   }
+
+   public T peek()
+   {
+   
+   }
+   
+   public boolean isEmpty()
+   {
+      
+      if (data[0] != null)
+      {
+         return false;
+      }
+      else
+      {
+         return true;
+      }
+   }
+
+   public void clear()
+   {
+      
+   }
 }
